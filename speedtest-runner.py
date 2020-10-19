cd9dd09c6eb3649f688547a5c58ddd4e8eb507c5
akierans <akierans@gmail.com>
2020-10-19 17:02:47 +0100

    initial commit

---
+++ speedtest-runner.py
@@ -0,0 +1,19 @@
+import speedtest
+
+servers = []
+# If you want to test against a specific server
+# servers = [1234]
+
+threads = None
+# If you want to use a single threaded test
+# threads = 1
+
+s = speedtest.Speedtest()
+
+s.download(threads=threads)
+s.upload(threads=threads)
+s.results.share()
+
+results_dict = s.results.dict()
+
+results_dict;
